5910183006cd0a0eecf0d32208533b1f6380aa1c
MarcioRamom <devgolangdev@gmail.com>
2024-04-07 13:28:34 -0400

    desafio_GO_DIO

---
+++ conversão.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var a int = 10
+var b string = "nome"
+
+func main() {
+
+	var c float64 = float64(a)
+	fmt.Printf("O valor de c:  %g e o valor de b: %s ", c, b)
+
+}
